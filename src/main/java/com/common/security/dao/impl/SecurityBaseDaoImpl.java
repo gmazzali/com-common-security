8d8c7015e8e9ee395e4e3096f929a274a23ab653
guillermomazzali <guillermomazzali@gmail.com>
2014-03-11 20:55:18 +0000

    Carga del DAO base para el framework.

---
+++ src/main/java/com/common/security/dao/impl/SecurityBaseDaoImpl.java
@@ -0,0 +1,33 @@
+package com.common.security.dao.impl;
+
+import java.io.Serializable;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.common.security.dao.SecurityBaseDao;
+import com.common.util.dao.impl.HibernateGenericDaoImpl;
+import com.common.util.model.Persistence;
+
+/**
+ * La clase que implementa la interfaz base para los DAOs dentro de este framework.
+ * 
+ * @since 05/02/2014
+ * @author Guillermo Mazzali
+ * @version 1.0
+ * 
+ * @param <E>
+ *            La clase que vamos a manejar dentro de este DAO.
+ * @param <PK>
+ *            La clase que corresponde al PK de la clase que vamos a manejar en este DAO.
+ */
+public abstract class SecurityBaseDaoImpl<E extends Persistence<PK>, PK extends Serializable> extends HibernateGenericDaoImpl<E, PK> implements
+		SecurityBaseDao<E, PK> {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		super.setSessionFactory(sessionFactory);
+	}
+}
